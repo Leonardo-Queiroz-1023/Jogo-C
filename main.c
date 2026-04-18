63bf8ceb112fca84ea8882eb1cd8f6ec1cf82bdf
Daniel andrade <moongames12345@gmail.com>
2026-04-18 15:45:08 -0300

    primeira parte1

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+// Define regras ou leis do jogo
+#define  CHUNK_SIZE 10000.0f
+#define 
+#define ARQUIVO_SCORES "scores.txt"
+
---
+++ player.c
@@ -0,0 +1,9 @@
+typedef struct Player {
+int vida;
+char nome[55];
+float Radiacao;//0-1000;
+int fome;//0-100;
+int sede;//0-10;
+float temperatura;//-50 a 50;
+
+}player;
