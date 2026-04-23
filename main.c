90000659d67a54136e4f971e3eb82bda5cfcdddb
Leonardo Queiroz <Leofilho229@gmail.com>
2026-04-23 10:00:41 -0300

    Configura Makefile e abre a janela principal com a interface inicial

--- main.c
+++ main.c
@@ -30,3 +30,39 @@ typedef struct Asteroide{
     bool ativo;
     struct Asteroide* prox;
 } Asteroide;
+
+int main(void){
+
+    const int larguraTela = 800;
+    const int alturaTela = 600;
+    InitWindow(larguraTela, alturaTela, "Defesa Celestial");
+
+    Camera3D camara = { 0 };
+    camara.position = (Vector3){ 0.0f, 0.0f, 0.0f }; 
+    camara.target = (Vector3){ 0.0f, 0.0f, 10.0f };  
+    camara.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camara.fovy = 45.0f;                             
+    camara.projection = CAMERA_PERSPECTIVE;
+
+    SetTargetFPS(60);
+
+    struct Player nave = { 100, 0 };
+
+    while (!WindowShouldClose()) {
+        
+        BeginDrawing();
+            ClearBackground(BLACK);
+
+            BeginMode3D(camara);
+                DrawGrid(20, 1.0f); 
+            EndMode3D();
+
+            DrawCircleLines(larguraTela / 2, alturaTela / 2, 5, LIME);
+            DrawText(TextFormat("VIDA: %d%%", nave.vida), 20, 20, 20, GREEN);
+            DrawText("RADAR OFF", 20, 50, 20, RED);
+
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
