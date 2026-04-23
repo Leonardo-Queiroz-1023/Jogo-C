6d6df116399187dcec23523eac8f4069f3c28014
Leonardo Queiroz <Leofilho229@gmail.com>
2026-04-23 08:57:24 -0300

    feat: criacao das structs de player, tiro e asteroide

--- main.c
+++ main.c
@@ -4,8 +4,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <stdbool.h>
 
 // Define regras ou leis do jogo
 #define MAXIMO FLT_MAX
 #define ARQUIVO_SCORES "planetas.txt"
 
+typedef struct Player{
+    int vida;
+    int pontuação;
+} player;
+
+typedef struct Tiro{
+    Vector3 posicao;
+    float velocidade;
+    float raio;
+    bool ativo;
+    struct Tiro* prox;
+} Tiro;
+
+typedef struct Asteroide{
+    Vector3 posicao;
+    float velocidade;
+    float raio;
+    bool ativo;
+    struct Asteroide* prox;
+} Asteroide;
