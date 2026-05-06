5db40989feed94463a8fcdc9e6663fffaaa7e90a
Daniel andrade <moongames12345@gmail.com>
2026-05-06 20:57:30 -0300

    lista

--- Salvar.c
+++ Salvar.c
@@ -1,4 +1,4 @@
-#define ARQIVO_PONTOS "DADOS.txt"
+#define ARQUIVO_PONTOS "DADOS.txt"
 #include "main.c"
 #define linhas 10
 #define colunas 2
@@ -18,5 +18,33 @@ static Lista* criarNo(int pontos,int tempo){
     NovoNo->pontos = pontos;
     NovoNo->tempo = tempo;
     NovoNo->prox = NULL;
-    return NovoNo
+    return NovoNo;
+}
+static void Add_crecente(Lista **head, int pontos, int tempo){
+    
+    Lista* novoNo= criarNo(pontos,tempo);
+    if(*head == NULL || pontos < (*head)->pontos){
+        novoNo->prox = *head;
+        *head = novoNo;
+        return;
+    }
+    Lista* atual = *head;
+    while(atual->prox != NULL && atual->prox->pontos < pontos){
+        atual = atual->prox;
+    }
+    novoNo->prox = atual->prox;
+    atual->prox = novoNo;
+}
+static void salvarPontos(Lista* head){
+    FILE* arquivo = fopen(ARQUIVO_PONTOS, "w");
+    if (arquivo == NULL){
+        printf("Erro ao abrir o arquivo para escrita.\n");
+        return;
+    }
+    Lista* atual = head;
+    while (atual != NULL){
+        fprintf(arquivo, "%d %d\n", atual->pontos, atual->tempo);
+        atual = atual->prox;
+    }
+    fclose(arquivo);
 }
